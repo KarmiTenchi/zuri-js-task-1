600c7240f03cf337db5e7aacf8f3fd397fd658bf
KarmiTenchi <73952811+KarmiTenchi@users.noreply.github.com>
2022-06-24 03:33:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let name = "Clinton Derrick Akpama"
+let height = "6 foot 1 inch"
+let Country = "Nigeria"
+
+alert ('Hello, my name is ' + name + '\nI am ' + height + 'tall. \nI am from ' + Country + '.')
+
